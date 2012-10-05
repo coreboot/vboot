supechicken/vboot_reference

Support alternative GPT header signature
In order to dual boot Windows and ChromeOS, Windows must
not find a GPT partition table on the disk. So change
ChromeOS to cope with an alternative signature "CHROMEOS"
instead of the standard "EFI PART"

BUG=chrome-os-partner:6108
TEST=rebuild chromeos, install it,
     dd if=/dev/sda of=/tmp/x bs=1k
     hexdump -C /tmp/X
     see the string CHROMEOS
BRANCH=link
Signed-off-by: Stefan Reinauer <reinauer@chromium.org>

Change-Id: Ia88eff33b9880bd73a78c1b8e026c1f8298c4557

firmware/lib/cgptlib/include/gpt.h
@@ -14,7 +14,8 @@
 
 __pragma(pack(push,1)) /* Support packing for MSVC. */
 
-#define GPT_HEADER_SIGNATURE "EFI PART"
+#define GPT_HEADER_SIGNATURE2 "EFI PART"
+#define GPT_HEADER_SIGNATURE  "CHROMEOS"
 #define GPT_HEADER_SIGNATURE_SIZE sizeof(GPT_HEADER_SIGNATURE)
 #define GPT_HEADER_REVISION 0x00010000
 

coreboot/vboot

     run cgpt legacy /dev/sda
Reviewed-on: https://gerrit.chromium.org/gerrit/31264
Reviewed-by: Randall Spangler <rspangler@chromium.org>
Commit-Ready: Stefan Reinauer <reinauer@chromium.org>
Tested-by: Stefan Reinauer <reinauer@chromium.org>
(cherry picked from commit b7b865cfee68190babd971ab9a897bdabbab075f)
Change-Id: I7388ba8c8621ba05b8bcec6d008e5b250feaa683
Reviewed-on: https://gerrit.chromium.org/gerrit/34787
Reviewed-by: Stefan Reinauer <reinauer@chromium.org>

firmware/lib/cgptlib/include/gpt.h
@@ -14,7 +14,8 @@
 
 __pragma(pack(push,1)) /* Support packing for MSVC. */
 
-#define GPT_HEADER_SIGNATURE "EFI PART"
+#define GPT_HEADER_SIGNATURE  "EFI PART"
+#define GPT_HEADER_SIGNATURE2 "CHROMEOS"
 #define GPT_HEADER_SIGNATURE_SIZE sizeof(GPT_HEADER_SIGNATURE)
 #define GPT_HEADER_REVISION 0x00010000
 