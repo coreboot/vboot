supechicken/vboot_reference

Add support for booting into alternative boot loaders
Add a generic way of selecting an alternative bootloader to run from the
developer-mode menu. This enables keys 1-9 to select a particular numbered
bootloader.

Adjust VbExLegacy() to take a numeric parameter to signal which boot
loader to run.

BUG=chromium:837018
BRANCH=none
TEST=FEATURES=test emerge-grunt --nodeps vboot_reference

Change-Id: I02eab1b87e21a6401ec42317c4c1fa1bd2767b53
Signed-off-by: Simon Glass <sjg@chromium.org>

firmware/stub/vboot_api_stub.c
@@ -164,7 +164,7 @@ enum VbEcBootMode_t VbGetMode(void)
 	return vboot_mode;
 }
 
-int VbExLegacy(void)
+int VbExLegacy(int bootldr_num)
 {
 	return 1;
 }

coreboot/vboot

CQ-DEPEND=CL:1228875
Reviewed-on: https://chromium-review.googlesource.com/1060854
Reviewed-by: Julius Werner <jwerner@chromium.org>
(cherry picked from commit 89a517730f0207dbef9b3ff219d360904dca456e)
Reviewed-on: https://chromium-review.googlesource.com/c/1277325
Reviewed-by: Martin Roth <martinroth@chromium.org>
Tested-by: Martin Roth <martinroth@chromium.org>

firmware/stub/vboot_api_stub.c
@@ -164,7 +164,7 @@ enum VbEcBootMode_t VbGetMode(void)
 	return vboot_mode;
 }
 
-int VbExLegacy(void)
+int VbExLegacy(int altfw_num)
 {
 	return 1;
 }