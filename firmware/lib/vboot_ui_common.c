0a29eb399a35c4d8e9e0473a448bbe3837e7d89b
Simon Glass <sjg@chromium.org>
2018-10-19 20:52:28 -0600

    Pass all calls to VBExLegacy() through a single function
    It is important that we lock the TPM before calling this function. We have
    several places where the function is called. Reduce the risk that the TPM
    is no locked by running all calls through a single point. Drop the
    vb2_exit_altfw() function as it is not needed now.
    
    We rely on being able to call RollbackKernelLock() multiple times since it
    ignores subsequent calls and does not attempt to lock the TPM twice.
    
    With the menu UI this causes a small change in behaviour: when starting
    legacy firmware fails the screen flashes AFTER the beep instead of before.
    Hopefully this difference is not important.
    
    Future work will unify the two UI more.
    
    BUG=chromium:837018
    BRANCH=none
    TEST=FEATURES=test emerge-grunt --nodeps vboot_reference
    
    Change-Id: I0ee0b52eb57c30c1e1bb4a7e60e11d060025ab17
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1292248
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- firmware/lib/vboot_ui_common.c
+++ firmware/lib/vboot_ui_common.c
@@ -7,6 +7,10 @@
 
 #include "sysincludes.h"
 
+#include "2sysincludes.h"
+#include "2common.h"
+
+#include "rollback_index.h"
 #include "vboot_api.h"
 #include "vboot_ui_common.h"
 
@@ -24,4 +28,13 @@ void vb2_error_beep(enum vb2_beep_type beep)
 		VbExBeep(120, 400);
 		break;
 	}
+}
+
+void vb2_run_altfw(int altfw_num)
+{
+	if (RollbackKernelLock(0))
+		VB2_DEBUG("Error locking kernel versions on legacy boot.\n");
+	else
+		VbExLegacy(altfw_num);	/* will not return if found */
+	vb2_error_beep(VB_BEEP_FAILED);
 }
