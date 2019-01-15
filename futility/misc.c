aa1bb20afdbec0bacfdc2aa4966bd4a4b790cdd2
Hung-Te Lin <hungte@chromium.org>
2019-01-15 18:07:23 +0800

    futility: updater: Correct HWID digest when preserving HWID
    Starting from GBB 1.2, a digest is stored in GBB and must be updated
    whenever the HWID string is changed.
    
    In shell script version of updater, the digest is automatically updated
    when we do "futility gbb -s --hwid=XXX", but in native updater
    implementation we only updated the HWID string and left digest
    unchanged, this leaves devices generating wrong PCR1 values.
    
    `cmd_gbb_utility` updates the digest by calling `update_hwid_digest`
    using vboot1 structure, so we should introduce a new vboot2 friendly
    function, `vb2_change_hwid`, which changes both HWID string and digest
    at same time.
    
    Note this has no impact for end user's devices with write protection
    enabled. Only changes dogfood units AU results.
    
    BUG=b:122248649
    TEST=make futil; tests/futility/run_test_scripts.sh $(pwd)/build/futility
    BRANCH=none
    
    Change-Id: I6ad2754e6df3c9dd66d71c560a2afc26d14eae33
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/1411950

--- futility/misc.c
+++ futility/misc.c
@@ -28,6 +28,7 @@
 #include "file_type.h"
 #include "futility.h"
 #include "gbb_header.h"
+#include "2struct.h"
 
 /* Default is to support everything we can */
 enum vboot_version vboot_version = VBOOT_VERSION_ALL;
@@ -166,6 +167,7 @@ int print_hwid_digest(GoogleBinaryBlockHeader *gbb,
 	return is_valid;
 }
 
+/* Deprecated. Use vb2_change_hwid in future. */
 /* For GBB v1.2 and later, update the hwid_digest field. */
 void update_hwid_digest(GoogleBinaryBlockHeader *gbb)
 {
@@ -179,6 +181,25 @@ void update_hwid_digest(GoogleBinaryBlockHeader *gbb)
 	vb2_digest_buffer(buf + gbb->hwid_offset, strlen(hwid_str),
 			  VB2_HASH_SHA256,
 			  gbb->hwid_digest, sizeof(gbb->hwid_digest));
+}
+
+int vb2_change_hwid(struct vb2_gbb_header *gbb, const char *hwid)
+{
+	uint8_t *to = (uint8_t *)gbb + gbb->hwid_offset;
+	int len = strlen(hwid);
+	if (len >= gbb->hwid_size)
+		return -1;
+
+	/* Zero whole area so we won't have garbage after NUL. */
+	memset(to, 0, gbb->hwid_size);
+	memcpy(to, hwid, len);
+
+	/* HWID digest must be updated since v1.2. */
+	if (gbb->major_version == 1 && gbb->minor_version < 2)
+		return 0;
+
+	update_hwid_digest((struct GoogleBinaryBlockHeader *)gbb);
+	return 0;
 }
 
 /*
