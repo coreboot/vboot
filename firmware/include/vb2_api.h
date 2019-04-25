719968c47a6220b3e660e4f72cbc2a56750eec00
Joel Kitching <kitching@google.com>
2019-04-25 18:21:16 +0800

    vboot: remove NEED_VB2_SHA_LIBRARY switch
    Replaced with the external-facing header vb2_sha.h.
    
    BUG=b:124141368, chromium:956474
    TEST=make clean && make runtests
    BRANCH=none
    
    Change-Id: Ib31c816ed8e2eb91c30080e854ec9245d59c98f2
    Signed-off-by: Joel Kitching <kitching@google.com>
    Cq-Depend: chromium:1599567, chromium:1583941, chromium:1610164
    Reviewed-on: https://chromium-review.googlesource.com/1583825
    Commit-Ready: Joel Kitching <kitching@chromium.org>
    Tested-by: Joel Kitching <kitching@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

--- firmware/include/vb2_api.h
+++ firmware/include/vb2_api.h
@@ -7,18 +7,9 @@
  *
  * DO NOT INCLUDE THE HEADERS BELOW DIRECTLY!  ONLY INCLUDE THIS FILE!
  *
- * Using vb2api.h as the single point of contact between calling firmware and
+ * Using vb2_api.h as the single point of contact between calling firmware and
  * vboot allows subsequent refactoring of vboot (renaming of headers, etc.)
  * without churning other projects' source code.
- */
-
-/*
- * Switches that can be used in conjunction with this header file:
- *
- * #define NEED_VB2_SHA_LIBRARY
- *   SHA library APIs may be called by external firmware as well as vboot.
- *   This is permissible because the SHA library routines below don't interact
- *   with the rest of vboot.
  *
  * #define NEED_VB20_INTERNALS
  *   Allows the caller to peek into vboot2 data structures, by including a
@@ -32,11 +23,6 @@
 
 /* Standard APIs */
 #include "../2lib/include/2api.h"
-
-/* SHA library */
-#ifdef NEED_VB2_SHA_LIBRARY
-#include "../2lib/include/2sha.h"
-#endif
 
 /*
  * Coreboot should not need access to vboot2 internals.  But right now it does.
