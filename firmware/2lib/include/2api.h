supechicken/vboot_reference

vboot/ui: Implement firmware log screen
Implement firmware log screen which can be accessed from advanced
options menu.

The screen displays a snapshot for the firmware log using the same
layout of debug info screen.

BRANCH=puff
BUG=b:146399181, b:146105976
TEST=CC=x86_64-pc-linux-gnu-clang;
    make clean && make runtests
TEST=USE="menu_ui" emerge-nami depthcharge
     select "advanced options",
     and navigate to firmware log screen

Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
Change-Id: I3cb5800d71925aa20ca4d5636172885e23fd0099

firmware/2lib/include/2api.h
@@ -1213,6 +1213,8 @@ enum vb2_screen {
 	VB2_SCREEN_LANGUAGE_SELECT		= 0x130,
 	/* Debug info */
 	VB2_SCREEN_DEBUG_INFO			= 0x140,
+	/* Firmware log */
+	VB2_SCREEN_FIRMWARE_LOG			= 0x141,
 	/* First recovery screen to select recovering from disk or phone */
 	VB2_SCREEN_RECOVERY_SELECT		= 0x200,
 	/* Invalid recovery media inserted */
@@ -1243,6 +1245,8 @@ enum vb2_ui_error {
 	VB2_UI_ERROR_DEV_MODE_ALREADY_ENABLED,
 	/* Debug info screen initialization failed */
 	VB2_UI_ERROR_DEBUG_LOG,
+	/* Firmware log screen initialization failed */
+	VB2_UI_ERROR_FIRMWARE_LOG,
 };
 
 /**
@@ -1328,6 +1332,18 @@ const char *vb2ex_get_debug_info(struct vb2_context *ctx);
  * @return The pointer to the vboot debug info string.  NULL on error.
  */
 char *vb2api_get_debug_info(struct vb2_context *ctx);
+
+/**
+ * Get the full firmware log string.
+ *
+ * Return a pointer to the full debug info string which is guaranteed to be
+ * null-terminated.  The function implementation should snapshot the full
+ * firmware log when it is called the first time.  Subsequent calls should
+ * return the same pointer.
+ *
+ * @return The pointer to the full firmware log string.  NULL on error.
+ */
+const char *vb2ex_get_firmware_log(void);
 
 /**
  * Specify the string to be used for an upcoming log screen display.

coreboot/vboot

BRANCH=puff, zork
TEST=USE="menu_ui" emerge-puff depthcharge
TEST=USE="menu_ui" emerge-zork depthcharge
TEST=select "advanced options",
Cq-Depend: chromium:2334490
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/2328241
Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
(cherry picked from commit 8196d4e598a86c31ac07c60de151d9e9c2f9502c)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/2393833
Reviewed-by: Furquan Shaikh <furquan@chromium.org>
Commit-Queue: Furquan Shaikh <furquan@chromium.org>
Tested-by: Furquan Shaikh <furquan@chromium.org>

firmware/2lib/include/2api.h
@@ -1293,6 +1293,8 @@ enum vb2_screen {
 	VB2_SCREEN_LANGUAGE_SELECT		= 0x130,
 	/* Debug info */
 	VB2_SCREEN_DEBUG_INFO			= 0x140,
+	/* Firmware log */
+	VB2_SCREEN_FIRMWARE_LOG			= 0x150,
 	/* First recovery screen to select recovering from disk or phone */
 	VB2_SCREEN_RECOVERY_SELECT		= 0x200,
 	/* Invalid recovery media inserted */
@@ -1323,6 +1325,8 @@ enum vb2_ui_error {
 	VB2_UI_ERROR_DEV_MODE_ALREADY_ENABLED,
 	/* Debug info screen initialization failed */
 	VB2_UI_ERROR_DEBUG_LOG,
+	/* Firmware log screen initialization failed */
+	VB2_UI_ERROR_FIRMWARE_LOG,
 	/* Untrusted confirmation */
 	VB2_UI_ERROR_UNTRUSTED_CONFIRMATION,
 };
@@ -1410,6 +1414,18 @@ const char *vb2ex_get_debug_info(struct vb2_context *ctx);
  * @return The pointer to the vboot debug info string.  NULL on error.
  */
 char *vb2api_get_debug_info(struct vb2_context *ctx);
+
+/**
+ * Get the full firmware log string.
+ *
+ * Return a pointer to the full firmware log string which is guaranteed to be
+ * null-terminated.  The function implementation should snapshot the full
+ * firmware log when it is called the first time.  Subsequent calls should
+ * return the same pointer.
+ *
+ * @return The pointer to the full firmware log string.  NULL on error.
+ */
+const char *vb2ex_get_firmware_log(void);
 
 /**
  * Specify the string to be used for an upcoming log screen display.