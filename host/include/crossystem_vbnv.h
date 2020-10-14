4dacbf92be28c937e07b397ade29e5b8a84cdb7f
Jack Rosenthal <jrosenth@chromium.org>
2020-10-13 19:04:52 -0600

    crossystem: remove mosys nvstorage implementation
    As promised, it's October 2020, nyan_kitty went AUE with M85, and M86
    just got pushed stable.  That means we can now delete this code :)
    
    BUG=chromium:1090803
    BRANCH=none
    TEST=compiles
    
    Signed-off-by: Jack Rosenthal <jrosenth@chromium.org>
    Change-Id: I5a15ef1e1ad02885af135d8e42d02d492bdd6c05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/2469604
    Reviewed-by: Paul Fagerburg <pfagerburg@chromium.org>

--- host/include/crossystem_vbnv.h
+++ host/include/crossystem_vbnv.h
@@ -28,20 +28,6 @@ int vb2_read_nv_storage_flashrom(struct vb2_context *ctx);
  */
 int vb2_write_nv_storage_flashrom(struct vb2_context* ctx);
 
-/**
- * Attempt to read non-volatile storage using mosys.
- *
- * Returns 0 if success, non-zero if error.
- */
-int vb2_read_nv_storage_mosys(struct vb2_context *ctx);
-
-/**
- * Attempt to write non-volatile storage using mosys.
- *
- * Returns 0 if success, non-zero if error.
- */
-int vb2_write_nv_storage_mosys(struct vb2_context* ctx);
-
 #ifdef __cplusplus
 }
 #endif
