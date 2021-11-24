033513a93a95192c5b28d517dc0898db4a7d0bcb
Yu-Ping Wu <yupingso@chromium.org>
2021-11-24 16:03:39 +0800

    ui: Fix default item for dev external boot screen
    Fix the default selected menu item for 2 screens in developer mode:
    
    - VB2_SCREEN_DEVELOPER_BOOT_EXTERNAL
    - VB2_SCREEN_DEVELOPER_INVALID_DISK
    
    Previously in CL:3195521 we forgot to explicitly set it in the init()
    and reinit() screen functions.
    
    BUG=b:207448560, b:207448571
    TEST=make run2tests
    BRANCH=none
    
    Change-Id: I1854502251238394a7e76ad9e676bae436ad0df5
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/3298657
    Reviewed-by: Henry Sun <henrysun@google.com>

--- firmware/2lib/2ui_screens.c
+++ firmware/2lib/2ui_screens.c
@@ -895,6 +895,8 @@ static const struct vb2_screen_info developer_to_norm_screen = {
 /******************************************************************************/
 /* VB2_SCREEN_DEVELOPER_BOOT_EXTERNAL */
 
+#define DEVELOPER_BOOT_EXTERNAL_ITEM_BACK 1
+
 static vb2_error_t developer_boot_external_init(struct vb2_ui_context *ui)
 {
 	vb2_error_t rv;
@@ -908,6 +910,7 @@ static vb2_error_t developer_boot_external_init(struct vb2_ui_context *ui)
 		return vb2_ui_screen_back(ui);
 	}
 
+	ui->state->selected_item = DEVELOPER_BOOT_EXTERNAL_ITEM_BACK;
 	rv = VbTryLoadKernel(ui->ctx, VB_DISK_FLAG_REMOVABLE);
 	/* If the status of the external disk doesn't match, skip the screen. */
 	if (rv != VB2_ERROR_LK_NO_DISK_FOUND)
@@ -918,7 +921,7 @@ static vb2_error_t developer_boot_external_init(struct vb2_ui_context *ui)
 
 static const struct vb2_menu_item developer_boot_external_items[] = {
 	LANGUAGE_SELECT_ITEM,
-	BACK_ITEM,
+	[DEVELOPER_BOOT_EXTERNAL_ITEM_BACK] = BACK_ITEM,
 	POWER_OFF_ITEM,
 };
 
@@ -934,9 +937,11 @@ static const struct vb2_screen_info developer_boot_external_screen = {
 /******************************************************************************/
 /* VB2_SCREEN_DEVELOPER_INVALID_DISK */
 
+#define DEVELOPER_INVALID_DISK_ITEM_BACK 1
+
 static const struct vb2_menu_item developer_invalid_disk_items[] = {
 	LANGUAGE_SELECT_ITEM,
-	BACK_ITEM,
+	[DEVELOPER_INVALID_DISK_ITEM_BACK] = BACK_ITEM,
 	POWER_OFF_ITEM,
 };
 
@@ -953,6 +958,7 @@ static vb2_error_t developer_invalid_disk_init(struct vb2_ui_context *ui)
 		return vb2_ui_screen_back(ui);
 	}
 
+	ui->state->selected_item = DEVELOPER_INVALID_DISK_ITEM_BACK;
 	rv = VbTryLoadKernel(ui->ctx, VB_DISK_FLAG_REMOVABLE);
 	/* If the status of the external disk doesn't match, skip the screen. */
 	if (rv == VB2_SUCCESS || rv == VB2_ERROR_LK_NO_DISK_FOUND)
