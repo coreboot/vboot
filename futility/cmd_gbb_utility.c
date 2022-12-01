6dc378ebd7eba33e560e1f2561e2e92c8690e326
Evan Benn <evanbenn@chromium.org>
2022-12-01 17:44:48 +1100

    futility: Move write_to_file to misc.c
    BUG=b:260531154
    BRANCH=None
    TEST=FEATURES=test emerge-grunt vboot_reference
    TEST=futility gbb -s --flags 0x0 /tmp/bios /tmp/bios2
    
    Change-Id: I92dc87acdd7f0efcb034ee1151fcae0651e80f6f
    Signed-off-by: Evan Benn <evanbenn@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4075307
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- futility/cmd_gbb_utility.c
+++ futility/cmd_gbb_utility.c
@@ -16,6 +16,7 @@
 #include <unistd.h>
 
 #include "futility.h"
+#include "updater_utils.h"
 
 static void print_help(int argc, char *argv[])
 {
@@ -251,41 +252,6 @@ fail:
 		fprintf(stderr, "ERROR: Unable to close %s: %s\n",
 			filename, strerror(errno));
 	return NULL;
-}
-
-static int write_to_file(const char *msg, const char *filename,
-			 uint8_t *start, size_t size)
-{
-	FILE *fp;
-	int r = 0;
-
-	fp = fopen(filename, "wb");
-	if (!fp) {
-		r = errno;
-		fprintf(stderr, "ERROR: Unable to open %s for writing: %s\n",
-			filename, strerror(r));
-		return r;
-	}
-
-	/* Don't write zero bytes */
-	if (size && 1 != fwrite(start, size, 1, fp)) {
-		r = errno;
-		fprintf(stderr, "ERROR: Unable to write to %s: %s\n", filename,
-			strerror(r));
-	}
-
-	if (0 != fclose(fp)) {
-		int e = errno;
-		fprintf(stderr, "ERROR: Unable to close %s: %s\n", filename,
-			strerror(e));
-		if (!r)
-			r = e;
-	}
-
-	if (!r && msg)
-		printf("%s %s\n", msg, filename);
-
-	return r;
 }
 
 static int read_from_file(const char *msg, const char *filename,
