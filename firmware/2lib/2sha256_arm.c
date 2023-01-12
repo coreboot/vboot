fa45847d86073841b935131c201e4f78c049b1fd
Yidi Lin <yidilin@chromium.org>
2023-01-12 14:49:43 +0800

    vboot: add arm64 SHA256 extension support
    Copy sha256_armv8a_ce_a64.S from [1] and make some small changes to make
    it fit in vboot_reference build environment.
    
    With this CL, the boot time gets 126ms improvement on Tentacruel.
    The `vboot kernel verification` is reduced to 12ms.
    
    [1]
    https://github.com/OP-TEE/optee_os/blob/master/core/arch/arm/crypto/sha256_armv8a_ce_a64.S
    
    BRANCH=corsola
    BUG=b:263514393
    TEST=make install_dut_test; ./build/tests/vb2_sha256_x86_tests
    TEST=check `cbmem -t` on Tentacruel
    
    Change-Id: Ic9abeae9687b2162d7ddadd46111ec20f34e771c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4170144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4660128
    Tested-by: LIU KUAN-FU <iamjeffliu@google.com>
    Reviewed-by: Yu-An Wang <wyuang@google.com>
    Commit-Queue: Yu-An Wang <wyuang@google.com>
    Auto-Submit: LIU KUAN-FU <iamjeffliu@google.com>

---
+++ firmware/2lib/2sha256_arm.c
@@ -0,0 +1,21 @@
+/* Copyright 2023 The ChromiumOS Authors
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * SHA256 implementation using ARMv8 Cryptography Extension.
+ */
+
+#include "2common.h"
+#include "2sha.h"
+#include "2sha_private.h"
+#include "2api.h"
+
+const uint32_t vb2_hash_seq[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+int sha256_ce_transform(uint32_t *state, const unsigned char *buf, int blocks);
+
+void vb2_sha256_transform_hwcrypto(const uint8_t *message,
+				   unsigned int block_nb)
+{
+	sha256_ce_transform(vb2_sha_ctx.h, message, block_nb);
+}
