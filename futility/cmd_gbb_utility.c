c316a942a54b0602418eadfa6d3f59e76ab48f8b
Edward O'Callaghan <quasisec@google.com>
2023-01-13 14:32:14 +1100

    futility/cmd_gbb_utility.c: Check wp status before flash write()
    Check write-protect status before attempting to write to SPI flash.
    
    BUG=b:238694831,b:260531154
    BRANCH=none
    TEST=```
    [..]
    SPI Configuration is locked down.
    FREG0: Flash Descriptor region (0x00000000-0x00000fff) is read-write.
    FREG1: BIOS region (0x003a0000-0x01ffffff) is read-write.
    FREG2: Management Engine region (0x00001000-0x0039ffff) is read-write.
    OK.
    Found Winbond flash chip "W25Q256JV_M" (32768 kB, Programmer-specific) on host.
    ERROR: write_to_flash: You must disable write protection before setting flags.
    ```
    
    Cq-Depend: chromium:4303349
    Change-Id: I46d72a8867292599bf1a7287d63c62999df41c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4161094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4381003
    Tested-by: Phoebe Wang <phoebewang@chromium.org>
    Auto-Submit: Phoebe Wang <phoebewang@chromium.org>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

--- futility/cmd_gbb_utility.c
+++ futility/cmd_gbb_utility.c
@@ -440,6 +440,10 @@ static int write_to_flash(struct updater_config *cfg, uint8_t *outbuf,
 			  off_t filesize)
 {
 #ifdef USE_FLASHROM
+	if (is_write_protection_enabled(cfg) == WP_ENABLED) {
+		ERROR("You must disable write protection before setting flags.\n");
+		return -1;
+	}
 	cfg->image.data = outbuf;
 	cfg->image.size = filesize;
 	int ret = write_firmware(cfg, &cfg->image, FMAP_RO_GBB);
