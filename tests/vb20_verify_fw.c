c978d4c3afb3e7250012daf6f25160df772ec689
Edward O'Callaghan <quasisec@google.com>
2023-04-06 08:36:52 +1000

    futility/updater: Fix legacy bios image naming
    Clean up many other instances of 'bios.bin' littered everywhere over
    time.
    
    BUG=b:65745723
    BRANCH=none
    TEST=`cros_run_unit_tests --host --packages vboot_reference`.
    
    Change-Id: Ie5916d003ba0a5873ce04ec96f48cd45c8253aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4402371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/4654126
    Tested-by: Chih-Yao Chuang <jasonchuang@google.com>
    Auto-Submit: Chih-Yao Chuang <jasonchuang@google.com>
    Commit-Queue: Chih-Yao Chuang <jasonchuang@google.com>
    Reviewed-by: Wen-Ting Tseng <wdzeng@chromium.org>

--- tests/vb20_verify_fw.c
+++ tests/vb20_verify_fw.c
@@ -19,7 +19,7 @@ const char *body_fname;
 
 /**
  * Local implementation which reads resources from individual files.  Could be
- * more elegant and read from bios.bin, if we understood the fmap.
+ * more elegant and read from image.bin, if we understood the fmap.
  */
 vb2_error_t vb2ex_read_resource(struct vb2_context *c,
 				enum vb2_resource_index index, uint32_t offset,
