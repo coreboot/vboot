b26c700ab9e260155910cf8eb8a6173a01b2db79
Yu-Ping Wu <yupingso@chromium.org>
2024-04-26 08:29:09 +0800

    scripts/image_signing/swap_ecrw: Support ecrw.version
    Add "ecrw.version" to FW_MAIN_A and FW_MAIN_B to allow easier tracking
    of the "ecrw" version in an AP firmwar image. Even if "ecrw.version"
    doesn't exist in the original AP image, the CBFS file will still be
    added.
    
    BUG=b:325886850
    TEST=make runtestscripts
    BRANCH=none
    
    Cq-Depend: chromium:5492530
    Disallow-Recycled-Builds: all
    Change-Id: Id2846fab6abedd039b6a7555b0c1de3a7674542b
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5492688
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>

--- tests/swap_ec_rw_tests.sh
+++ tests/swap_ec_rw_tests.sh
@@ -12,7 +12,9 @@
 set -e
 
 ME=${0##*/}
-TMP="${ME}.tmp"
+TMPD="${TEST_DIR}/${ME}"
+mkdir -p "${TMPD}"
+TMP="${TMPD}/image.bin"
 
 SWAP="${SRCDIR:?}/scripts/image_signing/swap_ec_rw"
 DATA="${SRCDIR:?}/tests/swap_ec_rw_data"
@@ -20,6 +22,7 @@ DATA="${SRCDIR:?}/tests/swap_ec_rw_data"
 # Intentionally use AP and EC images from different boards
 AP_IMAGE="${DATA}/bios_geralt.bin"
 EC_IMAGE="${DATA}/ec_krabby.bin"
+EC_RW_FWID="${DATA}/RW_FWID"
 
 echo "Testing swap_ec_rw..."
 
@@ -27,11 +30,19 @@ echo "Testing swap_ec_rw..."
 cp -f "${AP_IMAGE}" "${TMP}"
 if "${SWAP}" -i "${TMP}"; then false; fi
 
-# Good case
+# Good case: no ecrw.version
 cp -f "${AP_IMAGE}" "${TMP}"
 "${SWAP}" -i "${TMP}" -e "${EC_IMAGE}"
 cmp "${TMP}" "${DATA}/bios.expect.bin"
 
+# Good case: swap ecrw.version
+cp -f "${AP_IMAGE}" "${TMP}"
+cbfstool "${TMP}" add -r "FW_MAIN_A,FW_MAIN_B" -t raw \
+  -c none -f "${EC_RW_FWID}" -n ecrw.version
+"${SWAP}" -i "${TMP}" -e "${EC_IMAGE}"
+cbfstool "${TMP}" extract -r "FW_MAIN_A" -n ecrw.version -f "${TMPD}/v"
+cmp -s "${TMPD}/v" "${EC_RW_FWID}" && error "ecrw.version was not modified"
+
 # Cleanup
-rm -f "${TMP}"*
+rm -rf "${TMPD}"
 exit 0
