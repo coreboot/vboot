54446c196a2317d3854fc5f6ae7c91732f18b4ff
Dinesh Gehlot <digehlot@google.com>
2024-07-18 15:08:52 +0530

    2lib: Add gbb flag to enforce CSE sync
    The CSE sync process executes a CSE upgrade exclusively when the CSE
    version stored in CBFS CSE differs from the current CSE. Given the
    infrequency of CSE upgrades, we may not always able to test the cse
    sync flow. This patch incorporates a GBB flag, which, when enabled,
    enforces CSE sync even if the current CSE version matches the version
    in CBFS.
    
    BUG=b:353053317
    TEST=Able to build google/rex0.
    
    Change-Id: I4d3b56b2bd259ffd6b8b8c57ab35ad6a0f080de9
    Signed-off-by: Dinesh Gehlot <digehlot@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5718196
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5903139
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- firmware/2lib/2gbb.c
+++ firmware/2lib/2gbb.c
@@ -193,6 +193,10 @@ vb2_error_t vb2_get_gbb_flag_description(enum vb2_gbb_flag flag,
 		*name = "VB2_GBB_FLAG_ENABLE_UDC";
 		*description = "Enable USB Device Controller.";
 		break;
+	case VB2_GBB_FLAG_FORCE_CSE_SYNC:
+		*name = "VB2_GBB_FLAG_FORCE_CSE_SYNC";
+		*description = "Always sync CSE, even if it is same as CBFS CSE";
+		break;
 	default:
 		*name = NULL;
 		*description = NULL;
--- firmware/2lib/include/2gbb_flags.h
+++ firmware/2lib/include/2gbb_flags.h
@@ -93,6 +93,9 @@ enum vb2_gbb_flag {
 
 	/* Enable USB Device Controller */
 	VB2_GBB_FLAG_ENABLE_UDC = 1 << 16,
+
+	/* Enforce CSE SYNC, even if current CSE is same as CBFS CSE */
+	VB2_GBB_FLAG_FORCE_CSE_SYNC = 1 << 17,
 };
 
 vb2_error_t vb2_get_gbb_flag_description(enum vb2_gbb_flag flag,
