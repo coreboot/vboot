ded931d188bf8e9450eeca5260a6cf3dcc758674
Hung-Te Lin <hungte@chromium.org>
2024-09-21 01:20:35 +0800

    futility: updater: Process custom label as standard models
    One advantage of switching to signer_config.csv and getting rid of
    setvars.sh is we finally can know what custom label devices are
    supported.
    
    In `setvars.sh` or the pre-Unified-Build single package format, we have
    to scan the 'keyset/' folder to figure out what custom label tags may be
    available. But when using signer_config.csv, all the custom label names
    are already included in the signer information.
    
    As a result, we can finally remove the hacky way of patching custom
    label devices. One downside is we can no longer support custom label in
    non-Unified-Build systems, but all active Chromebooks are now Unified
    Build so that is not a problem. And for development using simple archive
    without `signer_config.csv`, there is no need to support custom label
    because we usually don't have signed keys.
    
    This is also the first step to remove the custom label logic from the
    firmware updater - preparation to let crosid handle custom label
    matching for us. This also implies `--signature_id` is now equivalent to
    `--model` and can be deprecated in the follow up changes.
    
    BUG=b:251040363
    TEST=make runfutiltests -j
    BRANCH=none
    
    (cherry picked from commit 7ad2b0ab50350df901de329a75aa40d189ef6f94)
    
    Change-Id: Ib4c62ba4d01b276472575625ea19397fc21b1589
    Original-Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5872452
    Original-Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    GitOrigin-RevId: 7ad2b0ab50350df901de329a75aa40d189ef6f94
    Cr-Build-Id: 8735377134969615841
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8735377134969615841
    Copybot-Job-Name: android-main-vboot_reference-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5886254
    Reviewed-by: Grzegorz Bernacki <bernacki@google.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>

--- futility/updater_quirks.c
+++ futility/updater_quirks.c
@@ -564,8 +564,8 @@ char *updater_get_cbfs_quirks(struct updater_config *cfg)
 	return (char *)data;
 }
 
-int quirk_override_signature_id(struct updater_config *cfg,
-				struct model_config *model,
+int quirk_override_signature_id(const struct updater_config *cfg,
+				const struct model_config *model,
 				const char **signature_id)
 {
 	const char * const DOPEFISH_KEY_HASH =
@@ -574,7 +574,7 @@ int quirk_override_signature_id(struct updater_config *cfg,
 	/* b/146876241 */
 	assert(model);
 	if (strcmp(model->name, "phaser360") == 0) {
-		struct firmware_image *image = &cfg->image_current;
+		const struct firmware_image *image = &cfg->image_current;
 		const char *key_hash = get_firmware_rootkey_hash(image);
 		if (key_hash && strcmp(key_hash, DOPEFISH_KEY_HASH) == 0) {
 			const char * const sig_dopefish = "phaser360-dopefish";
