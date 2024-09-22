90f591700475b68715f1f27fd30a0d712037cac6
Hung-Te Lin <hungte@chromium.org>
2024-09-22 11:33:10 +0800

    futility: updater: Add a new config 'output_only'
    There was no easy way to identify if the updater is running in
    output-only mode (`--mode=output`). However, some setup process
    (especially the custom label tag processing) will want to know if it is
    appropriate to read the system firmware. For example, the ODMs and
    factories will want to run `--mode=output` in the CrOS SDK to get
    the preflash image, and definitely not reading the host system firmware.
    
    After `--signature_id` is deprecated, specifying `--model` without
    tags will trigger reading system firmware. As a result, we need to
    identify if the updater is running in the output mode, and skip reading
    system firmware in that mode. Having the output_only will make it
    simpler instead of having the `strcmp(arg->mode, "output")` everywhere.
    
    BUG=b:251040363
    TEST=make runfutiltests -j
    BRANCH=None
    
    Change-Id: I5181c9a31df7db12e04fdd362fe02ca08be29916
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/5879038
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- futility/updater.c
+++ futility/updater.c
@@ -1481,8 +1481,7 @@ static int check_arg_compatibility(
 }
 
 static int parse_arg_mode(struct updater_config *cfg,
-			  const struct updater_config_arguments *arg,
-			  bool *do_output)
+			  const struct updater_config_arguments *arg)
 {
 	if (!arg->mode)
 		return 0;
@@ -1501,7 +1500,7 @@ static int parse_arg_mode(struct updater_config *cfg,
 		   strcmp(arg->mode, "factory_install") == 0) {
 		cfg->factory_update = 1;
 	} else if (strcmp(arg->mode, "output") == 0) {
-		*do_output = 1;
+		cfg->output_only = true;
 	} else {
 		ERROR("Invalid mode: %s\n", arg->mode);
 		return -1;
@@ -1638,7 +1637,6 @@ int updater_setup_config(struct updater_config *cfg,
 	int errorcnt = 0;
 	int check_wp_disabled = 0;
 	bool check_single_image = false;
-	bool do_output = false;
 	const char *archive_path = arg->archive;
 
 	/* Setup values that may change output or decision of other argument. */
@@ -1661,7 +1659,7 @@ int updater_setup_config(struct updater_config *cfg,
 	if (arg->try_update)
 		cfg->try_update = TRY_UPDATE_AUTO;
 
-	if (parse_arg_mode(cfg, arg, &do_output) < 0)
+	if (parse_arg_mode(cfg, arg) < 0)
 		return 1;
 
 	if (cfg->factory_update) {
@@ -1752,7 +1750,7 @@ int updater_setup_config(struct updater_config *cfg,
 		errorcnt += !!setup_config_quirks(arg->quirks, cfg);
 
 	/* Additional checks. */
-	if (check_single_image && !do_output && cfg->ec_image.data) {
+	if (check_single_image && !cfg->output_only && cfg->ec_image.data) {
 		errorcnt++;
 		ERROR("EC/PD images are not supported in current mode.\n");
 	}
@@ -1771,7 +1769,7 @@ int updater_setup_config(struct updater_config *cfg,
 	}
 
 	/* The images are ready for updating. Output if needed. */
-	if (!errorcnt && do_output) {
+	if (!errorcnt && cfg->output_only) {
 		const char *r = arg->output_dir;
 		if (!r)
 			r = ".";
