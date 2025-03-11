4134ab60ea706a0ac81731193868136f744fa0cb
Yu-Ping Wu <yupingso@chromium.org>
2025-03-11 18:37:16 +0800

    tests/futility/test_dump_fmap: Allow easier expected output updates
    Add a global variable UPDATE_MODE to allow updating the expected output
    files when set to 1.
    
    For consistency with other futility test scripts, replace `cmp` with
    `diff`.
    
    BUG=none
    TEST=make runfutiltests
    BRANCH=none
    
    Change-Id: Id42be74fcea25cbac5c4b7a3fc3dfe4cb874b76e
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6342557
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- tests/futility/test_dump_fmap.sh
+++ tests/futility/test_dump_fmap.sh
@@ -6,21 +6,32 @@
 me=${0##*/}
 TMP="${me}.tmp"
 
+# Set to 1 to update the expected output
+UPDATE_MODE=0
+
 # Work in scratch directory
 cd "${OUTDIR}"
 
+check_diff()
+{
+  local wantfile="$1"
+  local gotfile="$2"
+  [[ "${UPDATE_MODE}" -gt 0 ]] && cp "${gotfile}" "${wantfile}"
+  diff "${wantfile}" "${gotfile}"
+}
+
 # Good FMAP
 "${FUTILITY}" dump_fmap -F "${SCRIPT_DIR}/futility/data_fmap.bin"  > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_f.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_f.txt" "${TMP}"
 
 "${FUTILITY}" dump_fmap -p "${SCRIPT_DIR}/futility/data_fmap.bin"  > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_p.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_p.txt" "${TMP}"
 
 "${FUTILITY}" dump_fmap -h "${SCRIPT_DIR}/futility/data_fmap.bin"  > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_h.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_h.txt" "${TMP}"
 
 "${FUTILITY}" dump_fmap -e "${SCRIPT_DIR}/futility/data_fmap3.bin"  > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_e.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_e.txt" "${TMP}"
 
 
 # This should fail because the input file is truncated and doesn't really
@@ -35,24 +46,24 @@ if "${FUTILITY}" dump_fmap -x "${SCRIPT_DIR}/futility/data_fmap.bin" NO_SUCH; \
 # However, this should work.
 "${FUTILITY}" dump_fmap -x "${SCRIPT_DIR}/futility/data_fmap.bin" SI_DESC > \
   "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_x.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_x.txt" "${TMP}"
 
 # Redirect dumping to a different place
 "${FUTILITY}" dump_fmap -x "${SCRIPT_DIR}/futility/data_fmap.bin" SI_DESC:FOO \
   > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap_expect_x2.txt" "${TMP}"
-cmp SI_DESC FOO
+check_diff "${SCRIPT_DIR}/futility/data_fmap_expect_x2.txt" "${TMP}"
+diff SI_DESC FOO
 
 # This FMAP has problems, and should fail.
 if "${FUTILITY}" dump_fmap -h "${SCRIPT_DIR}/futility/data_fmap2.bin" > \
   "${TMP}"; then false; fi
-cmp "${SCRIPT_DIR}/futility/data_fmap2_expect_h.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap2_expect_h.txt" "${TMP}"
 
 "${FUTILITY}" dump_fmap -hh "${SCRIPT_DIR}/futility/data_fmap2.bin" > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap2_expect_hh.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap2_expect_hh.txt" "${TMP}"
 
 "${FUTILITY}" dump_fmap -hhH "${SCRIPT_DIR}/futility/data_fmap2.bin" > "${TMP}"
-cmp "${SCRIPT_DIR}/futility/data_fmap2_expect_hhH.txt" "${TMP}"
+check_diff "${SCRIPT_DIR}/futility/data_fmap2_expect_hhH.txt" "${TMP}"
 
 
 # cleanup
