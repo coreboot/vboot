9b362962860caef101b6d11debd2dd6366a2d274
Tomasz Michalec <tmichalec@google.com>
2025-07-11 15:25:41 +0200

    cgptlib: Make IsBootableEntry available for depthcharge
    Make IsBootableEntry, IsAndroid and IsChromeOS functions available for
    depthcharge, so fastboot code can distinguish different kinds of
    bootable partitions.
    
    BRANCH=main
    BUG=None
    TEST=Build AP FW
    
    Change-Id: I6561e121b934c52e811254185e7b91c392f8ff96
    Signed-off-by: Tomasz Michalec <tmichalec@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/vboot_reference/+/6728037
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- firmware/include/gpt_misc.h
+++ firmware/include/gpt_misc.h
@@ -145,6 +145,22 @@ uint64_t GptGetEntrySizeLba(const GptEntry *e);
  */
 uint64_t GptGetEntrySizeBytes(const GptData *gpt, const GptEntry *e);
 
+/**
+ * Return true if the entry is a Android VBMETA partition, else false.
+ */
+bool IsAndroid(const GptEntry *e);
+
+/**
+ * Return true if the entry is a ChromeOS kernel partition, else false.
+ */
+bool IsChromeOS(const GptEntry *e);
+
+/**
+ * Return true if the entry is a ChromeOS or Android partition,
+ * else false.
+ */
+bool IsBootableEntry(const GptEntry *e);
+
 /* Getters and setters for partition attribute fields. */
 
 int GetEntryRequired(const GptEntry *e);
--- firmware/lib/cgptlib/include/cgptlib_internal.h
+++ firmware/lib/cgptlib/include/cgptlib_internal.h
@@ -168,22 +168,6 @@ void GptRepair(GptData *gpt);
 void GptModified(GptData *gpt);
 
 /**
- * Return true if the entry is a Android VBMETA partition, else false.
- */
-bool IsAndroid(const GptEntry *e);
-
-/**
- * Return true if the entry is a ChromeOS kernel partition, else false.
- */
-bool IsChromeOS(const GptEntry *e);
-
-/**
- * Return true if the entry is a ChromeOS or Android partition,
- * else false.
- */
-bool IsBootableEntry(const GptEntry *e);
-
-/**
  * Copy the current kernel partition's UniquePartitionGuid to the dest.
  */
 void GetCurrentKernelUniqueGuid(GptData *gpt, void *dest);
